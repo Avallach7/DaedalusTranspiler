865d7f0e7ade56b9bdde21ed73372dadb87950f2
Avallach7 <ag.avallach+gt@gmail.com>
2016-01-21 23:46:33 +0100

    Initial commit - partially working bnf

---
+++ src/org/avallach/gothic/Transpiler.java
@@ -0,0 +1,11 @@
+package org.avallach.gothic;
+
+import org.avallach.commons.Debug;
+
+public class Transpiler
+{
+    public static void main(String[] args)
+    {
+		Debug.log("Hello World!");
+    }
+}
